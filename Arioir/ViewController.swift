fc9ee0e704855aa2614002539f0c377cf9a81f3b
Spiridonov Maxim <taijased@gmail.com>
2019-10-22 12:59:07 +0300

    Initial Commit

---
+++ Arioir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arioir
+//
+//  Created by Максим Спиридонов on 22.10.2019.
+//  Copyright © 2019 Максим Спиридонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
