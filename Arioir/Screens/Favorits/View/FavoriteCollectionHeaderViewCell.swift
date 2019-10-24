a68685f492669a5c24e3eb1eb6139a0b3dffe162
Spiridonov Maxim <taijased@gmail.com>
2019-10-24 17:29:16 +0300

    Добавление favoritsviewController

---
+++ Arioir/Screens/Favorits/View/FavoriteCollectionHeaderViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  FavoriteCollectionHeaderViewCell.swift
+//  Arioir
+//
+//  Created by Максим Спиридонов on 24.10.2019.
+//  Copyright © 2019 Максим Спиридонов. All rights reserved.
+//
+
+import Foundation
---
+++ Arioir/Screens/Favorits/View/FavoriteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteViewController.swift
+//  Arioir
+//
+//  Created by Максим Спиридонов on 24.10.2019.
+//  Copyright © 2019 Максим Спиридонов. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
